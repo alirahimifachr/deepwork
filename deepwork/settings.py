983c06403c01a3b126ad2a2ed153f0991450cf1c
alirahimifachr <30370078+alirahimifachr@users.noreply.github.com>
2022-04-14 19:09:01 +0200

    update settings.py

--- deepwork/settings.py
+++ deepwork/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'backend',
     'corsheaders',
     'rest_framework',
-    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -54,9 +53,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+# ]
 
 ROOT_URLCONF = 'deepwork.urls'
 
